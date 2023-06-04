067ee19fbf314e089619e49b693df6a5f8358562
Jack Lee <jack20499@gmail.com>
2023-06-04 21:53:16 +0800

    feat(UI): add mock text to Text Edit Battle Content
    * This commit adds lorem ipsum text to the Text Edit Battle Content
    for better UI readability and structure. The mock text serves as
    a placeholder to provide a more realistic representation of the
    final design
    
    * This change will help developers and designers visualise the
    layout and flow of the page more effectively

--- Pokemon/battleController.cpp
+++ Pokemon/battleController.cpp
@@ -22,6 +22,8 @@ BattleController::BattleController(Ui::PokemonClass* ui, QObject* parent)
 	connection();
 
 	ui->pb_battle_back->hide();
+	ui->text_edit_battle_content->setFontPointSize(15);
+	ui->text_edit_battle_content->setText("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nulla porttitor massa id neque aliquam vestibulum morbi blandit cursus. Magna etiam tempor orci eu lobortis elementum. Eget lorem dolor sed viverra. Tincidunt dui ut ornare lectus sit amet. Aliquam purus sit amet luctus venenatis lectus magna. Malesuada fames ac turpis egestas maecenas pharetra. Rhoncus aenean vel elit scelerisque. Felis imperdiet proin fermentum leo vel orci. Viverra nam libero justo laoreet sit amet cursus sit amet. Adipiscing enim eu turpis egestas pretium aenean pharetra magna ac. At auctor urna nunc id cursus metus aliquam. Faucibus vitae aliquet nec ullamcorper sit amet risus nullam eget. Tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada. Lorem mollis aliquam ut porttitor leo a diam sollicitudin.");
 }
 
 void BattleController::connection() {
